303aad43608d689f5d573bdbadd2a3ad7eeb7525
theoteno <49147230+theoteno@users.noreply.github.com>
2025-06-10 14:52:52 +0200

    Update battery_warning.sh
    English translation, battery alert at 25-80%

--- battery_warning.sh
+++ battery_warning.sh
@@ -2,7 +2,7 @@
 
 # Function to check if there's an existing Zenity dialog with "Low Battery Warning" or "High Battery Warning" title
 isZenityDialogOpen() {
-  wmctrl -l | grep 'Baterie se vybíjí!\|Baterie je nabitá!' > /dev/null
+  wmctrl -l | grep 'Battery is discharging!\|Battery is charged!' > /dev/null
 }
 
 # Run forever
@@ -20,15 +20,15 @@ while true; do
   adapter=$(cat /sys/class/power_supply/ADP0/online)
 
   # Low battery warning
-  if [[ "$battery" -lt 29 && "$adapter" == "0" ]]; then
+  if [[ "$battery" -lt 25 && "$adapter" == "0" ]]; then
     zenity --warning \
-      --title="Baterie se vybíjí!" \
-      --text="Stav Tvé baterie je ${battery}%! Připoj ji prosím do sítě." \
-      --ok-label="Jasně."
-  elif [[ "$battery" -gt 74 && "$adapter" == "1" ]]; then
+      --title="The battery is discharging!" \
+      --text="Battery charge is at ${battery}%! Plug in the charger." \
+      --ok-label="OK."
+  elif [[ "$battery" -gt 80 && "$adapter" == "1" ]]; then
     zenity --warning \
-      --title="Baterie je nabitá!" \
-      --text="Stav Tvé baterie je ${battery}%! Odpoj ji prosím ze sítě." \
+      --title="The battery is charged!" \
+      --text="Battery charge is at ${battery}%! Unplug the charger." \
       --ok-label="OK."
   fi
 
